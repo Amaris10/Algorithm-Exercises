5878a73baab0f94c2d2648e67abadd1a7287cd7a
Qiuyue Zhang <41809489+Amaris10@users.noreply.github.com>
2020-07-25 14:15:31 +0800

    王道机试chap2

---
+++ 0001.cpp
@@ -0,0 +1,20 @@
+/*0001 abc(Çå»ª¸´ÊÔ)*/
+#include<iostream>
+#include<cstdio>
+ 
+using namespace std;
+ 
+int main()
+{
+    for(int a = 0; a <= 9; ++a){
+        for(int b = 0;b <= 9; ++b){
+            for(int c = 0;c <= 9; ++c){
+                if(a*100+b*110+c*12==532){
+                    printf("%d %d %d\n",a,b,c);
+                }
+            }
+        }
+    }
+    return 0;
+}
+
